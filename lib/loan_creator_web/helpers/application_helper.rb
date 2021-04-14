83cd3dcde89168415663b79b77bc7a2e187d5ce7
Juliette Audema <juliette@capsens.eu>
2021-04-14 08:43:46 +0200

    Fix fix params When an empty attribute is sent by form, it becomes  with respond false for nil?, we have to check it's not there with empty?

--- lib/loan_creator_web/helpers/application_helper.rb
+++ lib/loan_creator_web/helpers/application_helper.rb
@@ -6,16 +6,16 @@ module LoanCreatorWeb
       {}.tap do |h|
         h[:type] = params[:type]
         h[:period] = params[:period]
-        h[:amount] = params[:amount].to_f unless params[:amount].nil?
-        h[:annual_interests_rate] = params[:annual_interests_rate].to_f unless params[:annual_interests_rate].nil?
-        h[:starts_on] = Date.parse(params[:starts_on]) unless params[:starts_on].nil?
-        h[:duration_in_periods] = params[:duration_in_periods].to_i unless params[:duration_in_periods].nil?
+        h[:amount] = params[:amount].to_f unless params[:amount].empty?
+        h[:annual_interests_rate] = params[:annual_interests_rate].to_f unless params[:annual_interests_rate].empty?
+        h[:starts_on] = Date.parse(params[:starts_on]) unless params[:starts_on].empty?
+        h[:duration_in_periods] = params[:duration_in_periods].to_i unless params[:duration_in_periods].empty?
         h[:initial_values] = {}.tap do |ivh|
-          ivh[:paid_capital] = params[:initial_values][:paid_capital].to_f unless params[:initial_values][:paid_capital].nil?
-          ivh[:paid_interests] = params[:initial_values][:paid_interests].to_f unless params[:initial_values][:paid_interests].nil?
-          ivh[:accrued_delta_interests] = params[:initial_values][:accrued_delta_interests].to_f unless params[:initial_values][:accrued_delta_interests].nil?
-          ivh[:due_interests] = params[:initial_values][:due_interests].to_f unless params[:initial_values][:due_interests].nil?
-          ivh[:starting_index] = params[:initial_values][:starting_index].to_i unless params[:initial_values][:starting_index].nil?
+          ivh[:paid_capital] = params[:initial_values][:paid_capital].to_f unless params[:initial_values][:paid_capital].empty?
+          ivh[:paid_interests] = params[:initial_values][:paid_interests].to_f unless params[:initial_values][:paid_interests].empty?
+          ivh[:accrued_delta_interests] = params[:initial_values][:accrued_delta_interests].to_f unless params[:initial_values][:accrued_delta_interests].empty?
+          ivh[:due_interests] = params[:initial_values][:due_interests].to_f unless params[:initial_values][:due_interests].empty?
+          ivh[:starting_index] = params[:initial_values][:starting_index].to_i unless params[:initial_values][:starting_index].empty?
         end
       end
     end
