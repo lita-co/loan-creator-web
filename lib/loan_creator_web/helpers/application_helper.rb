b072c05e36a49b50a407be94f890095d05180bda
Juliette Audema <juliette@capsens.eu>
2021-04-14 10:25:52 +0200

    Handle starting index in view and as passed parameters

--- lib/loan_creator_web/helpers/application_helper.rb
+++ lib/loan_creator_web/helpers/application_helper.rb
@@ -8,31 +8,42 @@ module LoanCreatorWeb
       {}.tap do |h|
         h[:type] = params[:type]
         h[:period] = params[:period]
-        h[:amount] = to_right_format(params: 'amount', value: params[:amount], type: 'Float')
-        h[:annual_interests_rate] = to_right_format(params: 'annual_interests_rate', value: params[:annual_interests_rate], type: 'Float')
-        h[:starts_on] = to_right_format(params: 'starts_on', value: params[:starts_on], type: 'Date')
-        h[:duration_in_periods] = to_right_format(params: 'duration_in_periods', value: params[:duration_in_periods], type: 'Integer')
+        h[:amount] = to_right_format(params: 'amount', value: params[:amount])
+        h[:annual_interests_rate] = to_right_format(params: 'annual_interests_rate', value: params[:annual_interests_rate])
+        h[:starts_on] = to_right_format(params: 'starts_on', value: params[:starts_on])
+        h[:duration_in_periods] = to_right_format(params: 'duration_in_periods', value: params[:duration_in_periods])
         h[:initial_values] = {}.tap do |ivh|
-          ivh[:paid_capital] = to_right_format(params: 'paid_capital', value: params[:initial_values][:paid_capital], type: 'Float')
-          ivh[:paid_interests] = to_right_format(params: 'paid_interests', value: params[:initial_values][:paid_interests], type: 'Float')
-          ivh[:accrued_delta_interests] = to_right_format(params: 'accrued_delta_interests', value: params[:initial_values][:accrued_delta_interests], type: 'Float')
-          ivh[:due_interests] = to_right_format(params: 'due_interests', value: params[:initial_values][:due_interests], type: 'Float')
-          ivh[:starting_index] = to_right_format(params: 'starting_index', value: params[:initial_values][:starting_index], type: 'Float')
+          ivh[:paid_capital] = to_right_format(params: 'paid_capital', value: params[:initial_values][:paid_capital])
+          ivh[:paid_interests] = to_right_format(params: 'paid_interests', value: params[:initial_values][:paid_interests])
+          ivh[:accrued_delta_interests] = to_right_format(params: 'accrued_delta_interests', value: params[:initial_values][:accrued_delta_interests])
+          ivh[:due_interests] = to_right_format(params: 'due_interests', value: params[:initial_values][:due_interests])
+          ivh[:starting_index] = to_right_format(params: 'starting_index', value: params[:initial_values][:starting_index])
         end
       end
     end
 
-    def to_right_format(params:, value:, type:)
-      return if value.blank?
-
-      case type
-      when 'Float'
+    def to_right_format(params:, value:)
+      case params
+      when *values_to_f
         value.to_f
-      when 'Date'
+      when 'starts_on'
         Date.parse(value)
-      when 'Integer'
+      when 'duration_in_periods'
         value.to_i
+      when 'starting_index'
+        value.to_i.zero? ? nil : value.to_i
       end
+    end
+
+    def values_to_f
+      %w(
+        amount
+        annual_interests_rate
+        paid_capital
+        paid_interests
+        accrued_delta_interests
+        due_interests
+      )
     end
 
     def selected(params:, value:)
@@ -41,6 +52,10 @@ module LoanCreatorWeb
 
     def number_to_euro(number)
       "#{number} €"
+    end
+
+    def initial_values_for_form(initial_value)
+      return nil if initial_value.nil? || initial_value.zero?
     end
   end
 end
